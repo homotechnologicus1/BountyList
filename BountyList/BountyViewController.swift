dc4b1197f29994bf392fcccccf02772567ba1bd5
joe <homo.technologicus1@gmail.com>
2020-05-24 19:06:33 +0900

    apply sortedList to the entire model

--- BountyList/BountyViewController.swift
+++ BountyList/BountyViewController.swift
@@ -80,7 +80,6 @@ class ListCell: UITableViewCell {
 }
 
 class BountyViewModel {
-    
     let bountyInfoList: [BountyInfo] = [
         BountyInfo(name: "brook", bounty: 33000000),
         BountyInfo(name: "chopper", bounty: 50),
@@ -92,12 +91,20 @@ class BountyViewModel {
         BountyInfo(name: "zoro", bounty: 120000000)
     ]
     
+    var sortedList: [BountyInfo] {
+        let sortedList = bountyInfoList.sorted { (prev, next) in
+            return prev.bounty > next.bounty
+        }
+        
+        return sortedList
+    }
+    
     var numOfBountyInfoList: Int {
         return bountyInfoList.count
     }
     
     func bountyInfo(at index: Int) -> BountyInfo {
-        return bountyInfoList[index]
+        return sortedList[index]
     }
     
 }
